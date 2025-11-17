de5243952f3cd4835fce64431f126d2125950fd3
sudarshan0001 <65151334+sudarshan0001@users.noreply.github.com>
2025-11-17 15:13:06 -0800

    Simplify GitHub Pages config without base paths

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  assetPrefix: '/Sudarshan-Portfolio',
-  basePath: '/Sudarshan-Portfolio'
+  }
 }
 
 module.exports = nextConfig
